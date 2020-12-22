b93f0e7064ef5404a1d53ed16d47fc8f77e38311
Chirag Gnanesh <73517004+chiragnanesh@users.noreply.github.com>
2020-12-22 14:58:27 +0530

    Add files via upload

--- form.js
+++ form.js
@@ -5,7 +5,23 @@ class Form{
 
     display(){
         var div = document.createElement('div');
-        div.innerHTML = 'Hi there!';
-        div.className = 'border pad';
+        div.id = "div1";
+        div.innerHTML = "helpline numbers";
+        document.body.appendChild(div);
+
+        var divTwo = document.createElement('div');
+        divTwo.id = "div2";
+        divTwo.innerHTML = "self defense";
+        document.body.appendChild(divTwo);
+
+        var divThree = document.createElement('div');
+        divThree.id = "div3";
+        divThree.innerHTML = "play a game";
+        document.body.appendChild(divThree);
+
+        var divFour = document.createElement('div');
+        divFour.id = "div4";
+        divFour.innerHTML = 'Hello';
+        document.body.appendChild(divFour);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 var backgroundImage;
 var form;
 
+let font;
+
 function preload(){
-    backgroundImage = loadImage("background/background.001.jpeg")
+    backgroundImage = loadImage("background/background.001.jpeg");
 }
 
 function setup(){
     createCanvas(windowWidth-800, windowHeight-150);
 
     form = new Form();
+    form.display();
 }
 
 function draw(){
     background(backgroundImage);
-    form.display();
+    
 } 
